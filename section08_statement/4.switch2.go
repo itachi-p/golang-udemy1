dc94ea992a5e6fbc71022973c8ed47871d096aa5
itachi-P <itachip38@gmail.com>
2023-05-01 15:54:38 +0900

    Finished switch statement.

--- section08_statement/4.switch2.go
+++ section08_statement/4.switch2.go
@@ -36,4 +36,39 @@ func main() {
 	// 型の変換には失敗してもエラー終了はしない
 	fmt.Println(f, isFloat64)
 
+	if x == nil {
+		fmt.Println("None")
+	} else if i, isInt := x.(int); isInt { // 右辺はisInt == trueの省略形
+		fmt.Println(i, "x is integer")
+	} else if s, isString := x.(string); isString {
+		fmt.Println(s, isString)
+	} else {
+		fmt.Println("I don't know.")
+	}
+
+	// 型によるswitch文
+	// if文による型アサーションよりも簡単に書けるので推奨
+	var y interface{} = "A"
+
+	// 変数yの型を調べ、処理を分ける
+	switch y.(type) {
+	case int:
+		fmt.Println("type: int")
+	case string:
+		fmt.Println("type: string")
+	default:
+		fmt.Println("I don't know.")
+	}
+
+	// 復元した値を使いたい場合
+	switch v := y.(type) {
+	case bool:
+		fmt.Println(v, "is boolean")
+	case int:
+		fmt.Println(v, "is integer")
+	case string:
+		fmt.Println(v, "is string")
+	}
+	/* 上記をそのまま関数anything()の中身にすれば、
+	受け取った引数の型によって処理を分ける関数が書ける。 */
 }
