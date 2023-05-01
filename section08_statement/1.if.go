f5326bfbfb230091d27350794a8576f7d764f14f
itachi-P <itachip38@gmail.com>
2023-05-01 11:13:35 +0900

    if statement (if and else)

--- section08_statement/1.if.go
+++ section08_statement/1.if.go
@@ -5,11 +5,13 @@ import "fmt"
 // if構文:条件分岐
 
 func main() {
-	a := 1
-	if a == 2 {
-		fmt.Println("two")
-	} else if a == 1 {
+	a, b := 1, 2
+	if a == 1 && b == 1 {
 		fmt.Println("one")
+	} else if a == 2 || b == 2 {
+		fmt.Println("two")
+	} else if a == 1 && b == 2 { // 上から順に評価される為、else構文ではここには入らない
+		fmt.Println("one & two")
 	} else {
 		fmt.Println("I don't know.")
 	}
