02b914fd66f7a6fb89283cd3749f471e23cd5d00
itachi-P <itachip38@gmail.com>
2023-05-02 15:53:28 +0900

    Panic and recover

--- section08_statement/7.panic_recover.go
+++ section08_statement/7.panic_recover.go
@@ -7,6 +7,18 @@ import "fmt"
 パニックよりもエラーハンドリングを使う方が推奨されている */
 
 func main() {
+
+	// 無名関数でリカバー処理
+	defer func() {
+		// パニック状態であれば変数xに値が返ってくる
+		if x := recover(); x != nil {
+			fmt.Println(x)
+		}
+	}()
+
+	// ランタイムを強制的に終了させる
+	/* 原則deferとセットで使うが、そもそもあまり出番がない。
+	同じ例外処理でもエラーハンドリングの方が推奨される。 */
 	panic("Runtime error")
 	fmt.Println("Start")
 }
