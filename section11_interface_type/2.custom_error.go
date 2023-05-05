bb4ed3a9040915bb6e5df0eac1a1b7f5fba54d57
itachi-P <itachip38@gmail.com>
2023-05-05 23:58:48 +0900

    interface : custom error

---
+++ section11_interface_type/2.custom_error.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// interface型
+// カスタムエラー
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ section11_interface_type/3.stringer.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// interface型
+// fmt.Stringer
+
+func main() {
+	fmt.Println("vim-go")
+}
